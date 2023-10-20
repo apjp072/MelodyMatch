d07baf1ee21cbb67646f7f5620aa1c0fa30c9969
apjp072 <apjpipod@gmail.com>
2023-10-20 16:05:35 -0500

    Initial Commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")] //will use UserController as the route, /api/users
+public class UsersController : ControllerBase
+{
+    private readonly DataContext _context;
+
+    public UsersController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        var users = await _context.Users.ToListAsync();
+        return users;
+
+    }
+
+    [HttpGet("{id}")] // /api/users/2
+    public async Task<ActionResult<AppUser>> GetUser(int id) //actionresult will return type AppUser
+    { 
+        return await _context.Users.FindAsync(id); //waiter waiting around in the kitchen for food to be ready
+    }
+ 
+}
