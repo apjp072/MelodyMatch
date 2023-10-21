fc9eea5870ed551b0df62524bf27b7974641a05e
apjp072 <apjpipod@gmail.com>
2023-10-21 16:44:12 -0500

    Adding Angular, Bootstrap, and https

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -3,31 +3,33 @@ using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace API.Controllers;
-
-[ApiController]
-[Route("api/[controller]")] //will use UserController as the route, /api/users
-public class UsersController : ControllerBase
+namespace API.Controllers
 {
-    private readonly DataContext _context;
 
-    public UsersController(DataContext context)
+    [ApiController]
+    [Route("api/[controller]")] //will use UserController as the route, /api/users
+    public class UsersController : ControllerBase
     {
-        _context = context;
-    }
+        private readonly DataContext _context;
 
-    [HttpGet]
-    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
-    {
-        var users = await _context.Users.ToListAsync();
-        return users;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
 
-    }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+            return users;
 
-    [HttpGet("{id}")] // /api/users/2
-    public async Task<ActionResult<AppUser>> GetUser(int id) //actionresult will return type AppUser
-    { 
-        return await _context.Users.FindAsync(id); //waiter waiting around in the kitchen for food to be ready
+        }
+
+        [HttpGet("{id}")] // /api/users/2
+        public async Task<ActionResult<AppUser>> GetUser(int id) //actionresult will return type AppUser
+        { 
+            return await _context.Users.FindAsync(id); //waiter waiting around in the kitchen for food to be ready
+        }
+    
     }
- 
 }
